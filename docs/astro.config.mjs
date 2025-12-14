9049f478fe9e46fdbfb5a6e0435feed8e5822c47
Marcus Schiesser <mail@marcusschiesser.de>
2025-12-14 19:45:06 +0700

    docs: cleaned deps

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -10,7 +10,7 @@ import remarkPackageInstall from "./src/plugins/remark-package-install.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://vectorstores.dev",
+	site: "https://vectorstores.org",
 	base: "/",
 	outDir: path.resolve("../dist/"),
 	markdown: {
@@ -24,21 +24,6 @@ export default defineConfig({
 				{
 					tag: "script",
 					content: `
-					(function (w, d, s, l, i) {
-					  w[l] = w[l] || [];
-					  w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
-					  var f = d.getElementsByTagName(s)[0],
-						j = d.createElement(s),
-						dl = l != "dataLayer" ? "&l=" + l : "";
-					  j.async = true;
-					  j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
-					  f.parentNode.insertBefore(j, f);
-					})(window, document, "script", "dataLayer", "GTM-WWRFB36R");
-				  `,
-				},
-				{
-					tag: "script",
-					content: `
 						document.addEventListener("DOMContentLoaded", function () {
 							var script = document.createElement("script");
 							script.type = "module";
@@ -49,12 +34,8 @@ export default defineConfig({
 							script.setAttribute("runllm-keyboard-shortcut", "Mod+j");
 							script.setAttribute("runllm-name", "vectorstores");
 							script.setAttribute("runllm-position", "BOTTOM_RIGHT");
-							script.setAttribute("runllm-assistant-id", "1450");
+							script.setAttribute("runllm-assistant-id", "1604");
 							script.setAttribute("runllm-disable-ask-a-person", true);
-							script.setAttribute(
-								"runllm-slack-community-url",
-								"https://discord.com/invite/eN6D2HQ4aX"
-							);
 							script.async = true;
 							document.head.appendChild(script);
 						});
@@ -110,19 +91,6 @@ export default defineConfig({
 		AutoImport({
 			imports: [
 				{
-					"@icons-pack/react-simple-icons": [
-						"SiBun",
-						"SiCloudflareworkers",
-						"SiDeno",
-						"SiNodedotjs",
-						"SiTypescript",
-						"SiVite",
-						"SiNextdotjs",
-						"SiDiscord",
-						"SiGithub",
-						"SiNpm",
-						"SiX",
-					],
 					"@astrojs/starlight/components": [
 						"Card",
 						"CardGrid",
